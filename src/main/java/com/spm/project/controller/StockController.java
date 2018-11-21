97d1351ba956488cc08493d99a9419bb50a6e560
jaskaran53 <30310889+jaskaran53@users.noreply.github.com>
2018-11-21 00:19:30 -0500

    adding mapping for method

--- src/main/java/com/spm/project/controller/StockController.java
+++ src/main/java/com/spm/project/controller/StockController.java
@@ -2,18 +2,20 @@ package com.spm.project.controller;
 
 import com.spm.project.service.StockService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
 
 @RestController
-@RequestMapping("/getStock")
+@RequestMapping("/stock")
 public class StockController {
 
     @Autowired
     private StockService stockService;
 
+    @GetMapping("/getPrice")
     public String getStockPrice() throws IOException {
         String stock = stockService.getStockPrice();
         return stock;
